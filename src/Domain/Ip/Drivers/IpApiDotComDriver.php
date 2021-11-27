a1b4c4b64c19af8e7c57fb2b20265d78a4ec24fb
AG <fdwnct@gmail.com>
2021-11-27 01:23:29 -0500

    add online driver tests to ensure correct exception upon auth failure

--- src/Domain/Ip/Drivers/IpApiDotComDriver.php
+++ src/Domain/Ip/Drivers/IpApiDotComDriver.php
@@ -2,11 +2,13 @@
 
 namespace XbNz\Resolver\Domain\Ip\Drivers;
 
+use Illuminate\Support\Facades\Cache;
 use XbNz\Resolver\Domain\Ip\Actions\GetApiKeysForDriverAction;
 use XbNz\Resolver\Domain\Ip\DTOs\IpData;
 use XbNz\Resolver\Domain\Ip\DTOs\QueriedIpData;
 use XbNz\Resolver\Support\Actions\MakeHttpCallAction;
 use XbNz\Resolver\Support\Drivers\Driver;
+use XbNz\Resolver\Support\Exceptions\ApiProviderException;
 
 class IpApiDotComDriver implements Driver
 {
@@ -40,14 +42,8 @@ class IpApiDotComDriver implements Driver
         return \Cache::remember(
             self::class . $ipData->ip,
             now()->addSeconds(config('resolver.cache_period')),
-            function () use ($ipData){
-                return $this->httpCallAction->execute(
-                    self::API_URL . "/{$ipData->ip}",
-                    $this,
-                    [
-                        'access_key' => \Arr::random($this->apiKeys),
-                    ]
-                )->json();
+            function () use ($ipData) {
+                return $this->handleHttpStatusCodeQuirk($ipData);
             }
         );
     }
@@ -60,6 +56,30 @@ class IpApiDotComDriver implements Driver
     public function requiresApiKey(): bool
     {
         return true;
+    }
+
+
+    /**
+     * This API provider has obliterated all REST conventions and is throwing a 200 code
+     * for failed authentication.
+     */
+    private function handleHttpStatusCodeQuirk(IpData $ipData): array
+    {
+        $response = $this->httpCallAction->execute(
+            self::API_URL . "/{$ipData->ip}",
+            $this,
+            [
+                'access_key' => \Arr::random($this->apiKeys),
+            ]
+        );
+
+        if (! isset($response->json()['success']) || $response->json()['success'] === true) {
+            return $response->json();
+        }
+
+        throw new ApiProviderException(
+            "{$this->supports()} has hit a snag and threw a {$response->status()} status code. Yes, 200: you saw that right. TWO HUNDRED. Go complain to the API devs."
+        );
     }
 
     public function requiresFile(): bool
--- tests/Feature/Ip/Drivers/IpApiDotComDriverTest.php
+++ tests/Feature/Ip/Drivers/IpApiDotComDriverTest.php
@@ -2,10 +2,13 @@
 
 namespace XbNz\Resolver\Tests\Feature\Ip\Drivers;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
 use XbNz\Resolver\Domain\Ip\Collections\IpCollection;
 use XbNz\Resolver\Domain\Ip\Drivers\IpApiDotComDriver;
 use XbNz\Resolver\Domain\Ip\Drivers\IpGeolocationDotIoDriver;
 use XbNz\Resolver\Resolver\Resolver;
+use XbNz\Resolver\Support\Exceptions\ApiProviderException;
 
 class IpApiDotComDriverTest extends \XbNz\Resolver\Tests\TestCase
 {
@@ -14,7 +17,7 @@ class IpApiDotComDriverTest extends \XbNz\Resolver\Tests\TestCase
      */
     public function it_resolves_and_caches_the_ip_information()
     {
-        \Config::set('resolver.cache_period', 3600);
+        Config::set('resolver.cache_period', 3600);
 
         $info = app(Resolver::class)
             ->ip()
@@ -24,5 +27,26 @@ class IpApiDotComDriverTest extends \XbNz\Resolver\Tests\TestCase
 
         $this->assertInstanceOf(IpCollection::class, $info);
         $this->assertTrue(\Cache::has(IpApiDotComDriver::class . '1.1.1.1'));
+    }
+
+    /** @test **/
+    public function provided_an_incorrect_api_key_it_throws_the_expected_exception(): void
+    {
+        Cache::flush();
+        Config::set('ip-resolver.api-keys.ipApiDotCom', ['wrong-api-key-should-be-refused']);
+
+        $this->expectException(ApiProviderException::class);
+
+        app(Resolver::class)
+            ->ip()
+            ->ipApiDotCom()
+            ->withIp('1.1.1.1')
+            ->normalize();
+
+
+        //TODO: Implement this type of fail-safe test for all other API drivers.
+        // Then move on to the 1.1 branch and try out the importers
+        // Maybe use the new lazy collection package to speed things up?!?
+
     }
 }
